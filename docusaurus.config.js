ac069878151874bf1dee61345894929f3f4245d6
metal <admin@metalruller.com>
2022-01-26 10:25:18 +0000

    config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,15 +6,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Zeppelin Selfhost Docs',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://zepdocs.i0.tf',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'zeppelinhangar', // Usually your GitHub org/user name.
+  projectName: 'selfhosting', // Usually your repo name.
 
   presets: [
     [
