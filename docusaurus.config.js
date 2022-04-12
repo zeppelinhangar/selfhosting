6870db935c7cafb062054a30b4b5de018be873e0
metal <metal@i0.tf>
2022-04-12 21:41:47 +0000

    move sidebars
    Signed-off-by: GitHub <noreply@github.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: require.resolve("./scripts/sidebars.js"),
           routeBasePath: "/",
         },
         theme: {
